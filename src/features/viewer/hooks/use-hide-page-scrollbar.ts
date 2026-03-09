b4e953a9cceb89657374333daa7787153f757991
alexander-bochkov <64841830+alexander-bochkov@users.noreply.github.com>
2026-03-09 12:52:25 +0400

    Add scrollbar hiding for the whole document

--- src/features/viewer/hooks/use-hide-page-scrollbar.ts
+++ src/features/viewer/hooks/use-hide-page-scrollbar.ts
@@ -5,7 +5,7 @@ const NO_SCROLL_CLASS = "no-scroll";
 export const useHidePageScrollbar = (hide: boolean) => {
   useEffect(() => {
     hide
-      ? document.body.classList.add(NO_SCROLL_CLASS)
-      : document.body.classList.remove(NO_SCROLL_CLASS);
+      ? document.documentElement.classList.add(NO_SCROLL_CLASS)
+      : document.documentElement.classList.remove(NO_SCROLL_CLASS);
   }, [hide]);
 };
