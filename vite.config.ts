2e059a3de4abd920f10ab74b28d5e50bec76d7ef
alexander-bochkov <64841830+alexander-bochkov@users.noreply.github.com>
2026-04-01 16:55:48 +0400

    Bump "@vitejs/plugin-react" version from 5.1.4 to 6.0.1 Bump "vite" version from 7.3.1 to 8.0.3 Bump "vite-plugin-svgr" version from 4.5.0 to 5.0.0 Remove "vite-tsconfig-paths" dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   build: {
-    rollupOptions: {
+    rolldownOptions: {
       input: {
         content: "src/main.tsx",
       },
@@ -15,5 +14,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), svgr(), tsconfigPaths()],
+  plugins: [react(), svgr()],
 });
